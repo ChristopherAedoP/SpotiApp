f91dac69a97714c07f3b3dffd1aa46b9a76866d9
Christopher Alexander Aedo Pizarro <Christopher.aedo.p@gmail.com>
2017-10-01 18:54:10 -0300

    primera version

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -11,7 +11,7 @@ export class SearchComponent implements OnInit {
 
   termino:string ="";
 
-  constructor(private _spotifyService:SpotifyService) { }
+  constructor(public _spotifyService:SpotifyService) { }
 
   ngOnInit() {
 
